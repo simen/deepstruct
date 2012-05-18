cbda13476ad80b6499a3c67ab0c4b19e99bbc812
Katrina Owen <katrina.owen@gmail.com>
2012-05-18 10:50:51 +0200

    Add support for predicate methods.
    Given the following:
    
        data = Deepstruct.wrap({:a => "something", :b => false})
    
        data.a?
        => true
    
        data.b?
        => false
    
        data.c?
        => false

--- lib/deepstruct.rb
+++ lib/deepstruct.rb
@@ -55,7 +55,10 @@ module DeepStruct
     def method_missing(method, *args, &block)
       return @value.send(method, *args, &block) if @value.respond_to?(method)
       method = method.to_s
-      if method.chomp!('=')        
+      if method.chomp!('?')
+        key = method.to_sym
+        @value.has_key?(key) && !!@value[key]
+      elsif method.chomp!('=')        
         raise ArgumentError, "wrong number of arguments (#{arg_count} for 1)", caller(1) if args.length != 1
         self[method] = args[0]
       elsif args.length == 0 && self.has_key?(method)

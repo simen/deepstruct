20287a0e59f142337d3b15276261e38e3b9f2637
Alexander Staubo <alex@bengler.no>
2014-05-01 10:59:23 -0400

    Fix spelling error indiffrently -> indifferently.

--- lib/deepstruct.rb
+++ lib/deepstruct.rb
@@ -42,23 +42,24 @@ module DeepStruct
     # Given a symbol or a string this yields the variant of the key that
     # exists in the wrapped hash if any. If none exists (or the key is not
     # a symbol or string) the input value is passed through unscathed.
-    def indiffrently(key, &block)
+    def indifferently(key, &block)
       return yield(key) if @value.has_key?(key)
       return yield(key.to_s) if key.is_a?(Symbol) && @value.has_key?(key.to_s)
       return yield(key.to_sym) if key.is_a?(String) && @value.has_key?(key.to_sym)
       return yield(key)
     end
+    alias_method :indiffrently, :indifferently  # Fixes spelling error
 
     def []=(key, value)
-      indiffrently(key) { |key| @value[key] = value }
+      indifferently(key) { |key| @value[key] = value }
     end
 
     def [](key)
-      indiffrently(key) { |key| DeepStruct.wrap(@value[key]) }
+      indifferently(key) { |key| DeepStruct.wrap(@value[key]) }
     end
 
     def has_key?(key)
-      indiffrently(key) { |key| @value.has_key?(key) }
+      indifferently(key) { |key| @value.has_key?(key) }
     end
 
     def method_missing(method, *args, &block)
